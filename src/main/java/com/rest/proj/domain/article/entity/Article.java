328bd5f5172b60eec935e330c6702e7959ce880a
0n0ng <hy991006@gmail.com>
2025-10-15 16:28:57 +0900

    article MVC

--- src/main/java/com/rest/proj/domain/article/entity/Article.java
+++ src/main/java/com/rest/proj/domain/article/entity/Article.java
@@ -1,10 +1,20 @@
 package com.rest.proj.domain.article.entity;
 
+import com.rest.proj.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
+@Entity
 @Getter
 @AllArgsConstructor
-public class Article {
-    private Long id;
+@NoArgsConstructor
+@ToString(callSuper = true)
+@SuperBuilder // extends(상속) 받으려면 Super사용
+public class Article extends BaseEntity {
+    private String subject;
+    private String content;
 }
