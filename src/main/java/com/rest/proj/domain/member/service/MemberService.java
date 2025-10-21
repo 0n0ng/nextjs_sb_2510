d5d62e9bb002c02e1e877d464b98ca9c42e3891a
0n0ng <hy991006@gmail.com>
2025-10-21 15:15:22 +0900

    member MVC set

---
+++ src/main/java/com/rest/proj/domain/member/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.rest.proj.domain.member.service;
+
+
+import com.rest.proj.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository membrerRepository;
+
+}
