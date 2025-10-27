91606e3c1feb094f199d3290eb6768b9c986a14b
0n0ng <hy991006@gmail.com>
2025-10-27 17:00:17 +0900

    article patch use Mutation

--- frontapp/src/app/article/[id]/page.tsx
+++ frontapp/src/app/article/[id]/page.tsx
@@ -1,27 +1,24 @@
 'use client'
 
 import { useParams } from 'next/navigation'
-import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useQuery } from '@tanstack/react-query'
-import { error } from 'console'
 import api from '@/src/utils/api'
 
 export default function ArticleDetail() {
   const params = useParams()
-  const [article, setArticle] = useState({})
+  const id = Array.isArray(params.id) ? params.id[0] : params.id
 
   const getArticle = async () => {
     return await api
       .get(`/articles/${params.id}`)
       .then((res) => res.data.data.article)
-      .catch((err) => console.log(err))
   }
 
   // 로딩, 에러, 데이터 받아왔을 때 각각 useQuery가 처리해줌
-  // 
+  //
   const { isLoading, error, data } = useQuery({
-    queryKey: ['article'],
+    queryKey: ['article', id],
     queryFn: getArticle,
   })
 
@@ -33,11 +30,13 @@ export default function ArticleDetail() {
     return (
       <>
         <h4>게시판 상세 {params.id}번</h4>
-        <div>{data.subject}</div>
-        <div>{data.content}</div>
-        <div>{data.createdDate}</div>
-        <div>{data.modifiedDate}</div>
+        <div>제목 : {data.subject}</div>
+        <div>작성자 : {data.author}</div>
+        <div>내용 : {data.content}</div>
+        <div>작성일 : {data.createdDate}</div>
+        <div>수정일 : {data.modifiedDate}</div>
         <Link href={`/article/${params.id}/edit`}>수정</Link>
+
       </>
     )
   }
